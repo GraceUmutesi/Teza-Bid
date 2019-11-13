13a6edb961324e451b8c5e3772adecc156766cc9
ahiodette <ahiode6@gmail.com>
2019-11-13 10:19:16 +0200

    Create login activity

---
+++ app/src/main/java/com/example/teza_bid/LoginActivityForBuyer.java
@@ -0,0 +1,14 @@
+package com.example.teza_bid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivityForBuyer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_for_buyer);
+    }
+}
