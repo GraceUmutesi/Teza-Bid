ad77f915e4f392415f47b19c8f1d5899ae3ec45a
GraceUmutesi <tesigrace98@gmail.com>
2019-11-14 19:06:31 +0200

    creating menu

---
+++ app/src/main/java/com/example/teza_bid/Drawer.java
@@ -0,0 +1,14 @@
+package com.example.teza_bid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Drawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+    }
+}
