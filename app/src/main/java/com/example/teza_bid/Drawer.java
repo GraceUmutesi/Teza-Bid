e4c1d9fc1f398fe2f625b8a82e3816c9a7252251
GraceUmutesi <tesigrace98@gmail.com>
2019-11-16 11:31:07 +0200

    navigation

--- app/src/main/java/com/example/teza_bid/Drawer.java
+++ app/src/main/java/com/example/teza_bid/Drawer.java
@@ -1,14 +1,31 @@
 package com.example.teza_bid;
-
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 public class Drawer extends AppCompatActivity {
-
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawer,R.string.open,R.string.close);
+        mDrawer.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/example/teza_bid/MainActivity.java
+++ app/src/main/java/com/example/teza_bid/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.teza_bid;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
