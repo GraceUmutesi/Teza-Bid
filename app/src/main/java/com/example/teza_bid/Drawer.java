464e523b982efb53ea25111a862f7b54d4035b35
GraceUmutesi <tesigrace98@gmail.com>
2019-11-16 17:20:19 +0200

    navigation fragment

--- app/src/main/java/com/example/teza_bid/Drawer.java
+++ app/src/main/java/com/example/teza_bid/Drawer.java
@@ -1,10 +1,15 @@
 package com.example.teza_bid;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 
 public class Drawer extends AppCompatActivity {
@@ -17,8 +22,10 @@ public class Drawer extends AppCompatActivity {
         mDrawer = (DrawerLayout) findViewById(R.id.drawer);
         mToggle = new ActionBarDrawerToggle(this,mDrawer,R.string.open,R.string.close);
         mDrawer.addDrawerListener(mToggle);
+        NavigationView nvDrawer =(NavigationView) findViewById(R.id.nav);
         mToggle.syncState();
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        setDrawerContent(nvDrawer);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
@@ -26,6 +33,40 @@ public class Drawer extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void selectItemDrawer(MenuItem menuItem){
+        Fragment myFrament = null;
+        Class fragClass;
+        switch (menuItem.getItemId()){
+            case R.id.nav_Buy:
+                fragClass = Categories.class;
+                break;
+
+            default:
+                fragClass = Categories.class;
+
+        }
+        try{
+            myFrament = (Fragment) fragClass.newInstance();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.catContent,myFrament);
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawer.closeDrawers();
+    }
+    private void setDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                selectItemDrawer(menuItem);
+                return true;
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/teza_bid/MainActivity.java
+++ app/src/main/java/com/example/teza_bid/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.teza_bid;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
