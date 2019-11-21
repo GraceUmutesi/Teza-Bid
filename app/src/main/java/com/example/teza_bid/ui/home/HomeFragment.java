a0168951a6ccde9921aaffff013c2da6f4d12b21
GraceUmutesi <tesigrace98@gmail.com>
2019-11-21 23:34:26 +0200

    all the last changes

--- app/src/main/java/com/example/teza_bid/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/teza_bid/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.example.teza_bid.ui.home;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -17,6 +20,7 @@ import com.example.teza_bid.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    SearchView mySearch;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +34,26 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+//        mySearch = (SearchView) root.findViewById(R.id.search);
+
+        AnimationDrawable animation = new AnimationDrawable();
+        animation.addFrame(getResources().getDrawable(R.drawable.dine), 3000);
+        animation.addFrame(getResources().getDrawable(R.drawable.bedr), 3000);
+        animation.addFrame(getResources().getDrawable(R.drawable.car), 3000);
+//        animation.addFrame(getResources().getDrawable(R.drawable.iphone), 3000);
+//        animation.addFrame(getResources().getDrawable(R.drawable.fri), 3000);
+        animation.addFrame(getResources().getDrawable(R.drawable.ca), 3000);
+//        animation.addFrame(getResources().getDrawable(R.drawable.dr), 3000);
+        animation.addFrame(getResources().getDrawable(R.drawable.jeans), 3000);
+//        animation.addFrame(getResources().getDrawable(R.drawable.me), 3000);
+        animation.addFrame(getResources().getDrawable(R.drawable.sa), 3000);
+//        animation.addFrame(getResources().getDrawable(R.drawable.sh), 3000);
+//        animation.addFrame(getResources().getDrawable(R.drawable.sui), 3000);
+//        animation.addFrame(getResources().getDrawable(R.drawable.wo), 3000);
+        animation.setOneShot(false);
+        ImageView imageAnim = (ImageView) root.findViewById(R.id.image);
+        imageAnim.setBackgroundDrawable(animation);
+        animation.start();
         return root;
     }
 }
