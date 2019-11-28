4b083b4fea0c7e687283ae98a1016362fe9afed9
Nyirate <rwakaemma34@gmail.com>
2019-11-28 13:17:09 +0200

    add all

--- app/src/main/java/com/example/teza_bid/PayPal.java
+++
@@ -1,5 +0,0 @@
-package com.example.teza_bid;
-
-public class PayPal {
-    public static final String PAYPAL_CLIENT_ID = "YOUR PAYPAL CLIENT ID";
-}
--- app/src/main/java/com/example/teza_bid/RentFormActivity.java
+++ app/src/main/java/com/example/teza_bid/RentFormActivity.java
@@ -1,14 +1,82 @@
 package com.example.teza_bid;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class RentFormActivity extends AppCompatActivity {
 
+    TextView textView;
+    Button mButton;
+    ImageView imageView;
+    String Information[] = {"Price:120,00Rwf/day" , "Price:100,00Rwf/day", "Price:200,00Rwf/day", "Price:170,00Rwf/day", "Price:300,00Rwf/day", "Price:350,00Rwf/day"};
+    int image[]={R.drawable.chair7,
+            R.drawable.chair4,
+            R.drawable.chair6,
+            R.drawable.chair3,
+            R.drawable.chair5,
+            R.drawable.chair1,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rent_form);
+        textView =(TextView) findViewById(R.id.desc);
+        imageView = (ImageView) findViewById(R.id.img);
+        mButton = (Button) findViewById(R.id.but);
+
+
+
+           mButton.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   Intent rent = new Intent(RentFormActivity.this,ContactActivity.class);
+                   startActivity(rent);
+
+               }
+           });
+
+
+            }
+
+    class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String Information[];
+        int image[];
+        MyAdapter (Context c , String information[], int images[]) {
+            super(c,R.layout.activity_rent,R.id.desc,information);
+            this.context = c;
+            this.Information= information;
+            this.image = images;
+
+        }
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater =(LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.activity_rent_form,parent,false);
+            ImageView images = row.findViewById(R.id.img);
+            TextView myDescription = row.findViewById(R.id.desc);
+            images.setImageResource(image[position]);
+            myDescription.setText(Information[position]);
+
+
+            return row;
+        }
     }
+
+
 }
